1a7dd1084eba3231388a045bc31287e4105aa5b4
Huy Phan <huy.phan@mesoneer.io>
2023-10-21 15:40:32 +0700

    Modify some basic information

---
+++ config/server.ts
@@ -0,0 +1,11 @@
+export default ({ env }) => ({
+  url: env('PUBLIC_URL', 'http://localhost'),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+});
