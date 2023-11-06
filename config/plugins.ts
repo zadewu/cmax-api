419eaf35c10baf49bc414b8a3ac3c2295d056469
Huy Phan <pbhuy93@gmail.com>
2023-11-06 22:53:45 +0700

    Change responsive image breakpoint

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,10 +2,8 @@ export default ({ env }) => ({
   upload: {
     config: {
       breakpoints: {
-        xlarge: 1920,
         large: 1000,
         medium: 750,
-        small: 500
       }
     }
   },
